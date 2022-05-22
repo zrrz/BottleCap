4f784b50a87d5c5770d2d5bde952dd0e7e520b4d
TomMatney <matney.tommy@gmail.com>
2022-05-22 12:42:52 -0400

    Fixes

---
+++ BottleCap/Assets/_Game/Scripts/UI/CreditScript/CreditScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditScript : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject credits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void TaskOnClick()
+    {
+        credits.SetActive(true);
+    }
+}
